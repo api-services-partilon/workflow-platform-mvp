66e06cfc15b1ccaedc534d10ca9572da6a96a837
Todsawat-Somtua <todsawat.st@gmail.com>
2025-05-16 10:01:55 +0700

    install vercel insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Toaster } from "sonner";
 import { QueryProvider } from "@/components/query-provider";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -24,6 +24,7 @@ export default function RootLayout({
       <body className={cn(inter.className, "antialiased min-h-screen")}>
         <QueryProvider>
           <Toaster />
+          <SpeedInsights />
           {children}
         </QueryProvider>
       </body>
